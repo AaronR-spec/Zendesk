7ac7498eec43234137d4cd031626e065134d9ba8
AaronR-spec <aaronr.reihill@gmail.com>
2020-11-14 12:01:39 +0000

    Api error
    error handling for the api if request is not sent then returns
    200, a 404 problem is displayed on the cli

--- ticket.py
+++ ticket.py
@@ -1,16 +1,14 @@
-class Ticket():
+class Ticket:
 
     def __init__(self, ticket_id, created_date, updated_date, ticket_type, subject, priority, status):
         self.ticket_id = ticket_id
         self.created_date = created_date
         self.updated_date = updated_date
-        self.ticket_type = ticket_type
-        self.subject = subject
-        self.priority = priority
-        self.status = status
+        self.ticket_type = str(ticket_type)
+        self.subject = str(subject)
+        self.priority = str(priority)
+        self.status = str(status)
 
     def display(self):
-        print(str(self.ticket_id) + ", " + self.created_date + ", " + self.updated_date + ", " + str(self.ticket_type) + ", "
-              + self.subject + ", " + str(self.priority) + ", " + self.status)
-
-
+        print(str(self.ticket_id) + ", " + self.created_date + ", " + self.updated_date + ", " + self.ticket_type + ", "
+              + self.subject + ", " + self.priority + ", " + self.status)
--- viewer.py
+++ viewer.py
@@ -5,10 +5,47 @@ import requests
 class Viewer:
 
     def __init__(self):
-        self.tickets = self.get_tickets()
+        self.tickets = self.__get_tickets()
 
-    def get_tickets(self):
-        url = 'https://aaronreihill.zendesk.com/api/v2/tickets.json'
+    def get_ticket(self, ticket_id):
+        start = 0
+        end = len(self.tickets) - 1
+
+        while start <= end:
+            mid = start + (end - start) // 2
+            current = self.tickets[mid]
+            if current.ticket_id == ticket_id:
+                return current
+            elif ticket_id < current.ticket_id:
+                end = mid - 1
+            else:
+                start = mid + 1
+        print("Ticket Not Found")
+
+    def add(self, ticket):
+        self.tickets.append(ticket)
+
+    def display(self):
+        if len(self.tickets) <= 0:
+            print("Nothing To Display")
+            return
+        amount_tickets = len(self.tickets)
+        record_index = 0
+        i = 0
+        if amount_tickets > 25:
+            i = 0
+        while i < amount_tickets:
+            if record_index > 25:
+                user = input("Next page Y/N: ")
+                if user.lower() != 'y':
+                    break
+                record_index = 0
+            self.tickets[i].display()
+            record_index = record_index + 1
+            i += 1
+
+    def __get_tickets(self):
+        url = 'https://aaronereihill.zendesk.com/api/v2/tickets.json'
         user = 'd00222467@student.dkit.ie'
         pwd = 'aaronreihillzendesk'
 
@@ -17,14 +54,14 @@ class Viewer:
 
         # Check for HTTP codes other than 200
         if response.status_code != 200:
-            print('Status:', response.status_code, 'Problem with the request. Exiting.')
-
+            print('Status:', response.status_code, 'Problem with the request ro the API.')
+            return []
         # Decode the JSON response into a dictionary and use the data
         data = response.json()
-        return self.create_tickets(data)
+        return self.__create_tickets(data)
 
     @staticmethod
-    def create_tickets(data):
+    def __create_tickets(data):
         ticket_list = []
         for d in data['tickets']:
             # #    # ticket_id, created_date, updated_date, ticket_type, subject, priority, status):
@@ -38,33 +75,3 @@ class Viewer:
             ticket = Ticket(ticket_id, created_date, updated_date, ticket_type, subject, priority, status)
             ticket_list.append(ticket)
         return ticket_list
-
-    def ticket_by_id(self, ticket_id):
-        start = 0
-        end = len(self.tickets) - 1
-
-        while start <= end:
-            mid = start + (end - start) // 2
-            current = self.tickets[mid]
-            if current.ticket_id == ticket_id:
-                return current
-            elif ticket_id < current.ticket_id:
-                end = mid - 1
-            else:
-                start = mid + 1
-        return None
-
-    def display(self):
-        amount_tickets = len(self.tickets)
-        record_index = 0
-        if amount_tickets > 25:
-            i = 0
-        while i < amount_tickets:
-            if record_index > 25:
-                user = input("Next page Y/N: ")
-                if user.lower() != 'y':
-                    break
-                record_index = 0
-            self.tickets[i].display()
-            record_index = record_index + 1
-            i += 1
