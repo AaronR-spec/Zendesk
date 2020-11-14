9982b233677140e586f00072dfa2e4b2f398d2f9
AaronR-spec <aaronr.reihill@gmail.com>
2020-11-14 17:36:42 +0000

    Formated menu
    string formated table of tickets to make it easy to read

--- main.py
+++ main.py
@@ -4,23 +4,28 @@ from viewer import Viewer
 def menu():
     ticket_viewer = Viewer()
     print("\nZendesk Ticket Viewer")
+    ticket_display_header = "{:<5} {:<10} {:<10} {:<10} {:<14} {:<14} {:<60}".format("Id", "Type", "Priority",
+                                                                                     "Status", "Updated",
+                                                                                     "Created", "Subject")
     while True:
         print("\n1.Display Tickets \n2.Display Ticket By Id \n3.Exit/Quit")
         user = input("Option: ")
 
         if user == '1':
+            print(ticket_display_header, '\n', '-' * 74)
             ticket_viewer.display()
         elif user == '2':
             try:
                 ticket_id = int(input("Id: "))
+                print(ticket_display_header, '\n', '-' * 74)
                 ticket_viewer.get_ticket(ticket_id)
             except ValueError:
-                print("Id entered is not a valid Id, please try again...")
+                print("Id entered is not a valid, please try again..")
         elif user == '3':
             print("Goodbye... Have a good day")
             break
         else:
-            print("Sorry " + user + " was not an option...")
+            print("Sorry " + user + " was not an option..")
 
 
 if __name__ == "__main__":
--- ticket.py
+++ ticket.py
@@ -1,3 +1,4 @@
+
 class Ticket:
 
     def __init__(self, ticket_id, created_date, updated_date, ticket_type, subject, priority, status):
@@ -10,5 +11,6 @@ class Ticket:
         self.status = str(status)
 
     def display(self):
-        print(str(self.ticket_id) + ", " + self.created_date + ", " + self.updated_date + ", " + self.ticket_type + ", "
-              + self.subject + ", " + self.priority + ", " + self.status)
+        print("{:<5} {:<10} {:<10} {:<10} {:<14} {:<14} {:<60}"
+              .format(str(self.ticket_id), self.ticket_type, self.priority,
+                      self.status, str(self.updated_date), str(self.created_date), self.subject))
